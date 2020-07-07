83ef0ffc2a8f8b1c15a78e6fcd9ffeb3a49d76fd
David Musko <davmusko@gmail.com>
2020-07-07 05:03:01 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: 'hi',
+    };
+  }
+
+  render() {
+    const { hello } = { ...this.state };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            <code>src/App.js</code>
+            and save to reload.
+            <p>{hello}</p>
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
